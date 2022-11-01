70154dd4b9ad1726eea57e8be4276f8be1376d8b
caishifang <fangcsyuan79@163.com>
2022-11-01 15:04:37 +0800

    finish request

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import TDesign from 'tdesign-vue-next'
+import 'tdesign-vue-next/dist/reset.css'
+import 'tdesign-vue-next/es/style/index.css'
 
 createApp(App).use(router).use(TDesign).mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,19 +3,35 @@
     <div class="content">
       <t-card>
         <h1>Admin</h1>
-        <t-form ref="form" :colon="true" :label-width="0">
-          <t-form-item name="account">
-            <t-input clearable placeholder="请输入账户名">
+        <t-form
+          ref="form"
+          :colon="true"
+          :label-width="0"
+          class="login-form"
+          :data="loginForm"
+          :rules="rules"
+        >
+          <t-form-item name="username">
+            <t-input
+              clearable
+              placeholder="请输入用户名"
+              v-model="loginForm.username"
+            >
               <template #prefix-icon>
-                <desktop-icon />
+                <icon name="desktop" />
               </template>
             </t-input>
           </t-form-item>
 
           <t-form-item name="password">
-            <t-input type="password" clearable placeholder="请输入密码">
+            <t-input
+              type="password"
+              clearable
+              placeholder="请输入密码"
+              v-model="loginForm.password"
+            >
               <template #prefix-icon>
-                <lock-on-icon />
+                <icon name="lock-on" />
               </template>
             </t-input>
           </t-form-item>
@@ -29,13 +45,49 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icon } from 'tdesign-vue-next'
+import type { TokenRequest } from '@/api/types'
+import { reactive } from 'vue'
 
-<style scoped>
+const loginForm = reactive<TokenRequest>({
+  username: '',
+  password: ''
+})
+
+const rules = {
+  username: [
+    {
+      required: true,
+      message: '请填写用户名'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: '请填写密码'
+    }
+  ]
+}
+</script>
+
+<style scoped lang="less">
 .login-container {
   width: 100vw;
   height: 100vh;
-  background-color: skyblue;
-  margin: 0;
+  background-color: #e5e5e5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .content {
+    width: 400px;
+    h1 {
+      text-align: center;
+    }
+
+    .login-form {
+      margin: 20px 0;
+    }
+  }
 }
 </style>
